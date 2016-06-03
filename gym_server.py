e5b19d2296732007aa9232d54430049ccec36483
Flavio Sales Truzzi <flaviotruzzi@gmail.com>
2016-06-03 20:40:21 +0200

    Lots of improvements on the API. Based on the work of @catherio

--- gym_server.py
+++ gym_server.py
@@ -1,45 +1,10 @@
-import os
-
 import gym
-from PIL import Image
-from flask import Flask, jsonify, render_template, request, send_file
+from flask import Flask, jsonify, request
 
-environment = os.environ['GYM_ENV']
+from EnvironmentManager import EnvironmentManager
+from Exceptions import InvalidUsage
 
-env = gym.make(environment)
-
-
-class InvalidUsage(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-
-
-def get_render_mode(environ):
-    if 'rgb_array' in environ.metadata['render.modes']:
-        return 'rgb_array'
-    elif 'ansi' in environ.metadata['render.modes']:
-        return 'ansi'
-    else:
-        return None
-
-
-def save_img(img):
-    im = Image.fromarray(img)
-    im.save("simulation.png")
-
-
-render_mode = get_render_mode(env)
+envs = EnvironmentManager()
 
 app = Flask(__name__)
 
@@ -51,59 +16,126 @@ def handle_invalid_usage(error):
     return response
 
 
-@app.route("/reset")
-def reset():
+@app.route('/v1/envs/create/', methods=['POST'])
+def create_environment():
+    """
+    Create Environment. Accept a json with the following format:
+    {
+      'environment': 'CartPole-v0'
+    }
+
+    :return: json with instance_id of the environment
+    """
+    environment = request.get_json()['environment']
+    instance_id = envs.create(environment)
+    return jsonify(instance_id=instance_id), 201
+
+
+@app.route('/v1/envs/<instance_id>/reset/', methods=['GET'])
+def reset(instance_id):
     """
     Reset environment.
+
+    :param instance_id:
     :return: first observation
     """
-    data = env.reset().tolist()
-    save_img(env.render(render_mode))
-    return jsonify({'observation': data})
+    return jsonify(observation=envs.reset(instance_id))
 
 
-@app.route("/step/<int:action>")
-def step(action):
+@app.route('/v1/envs/<instance_id>/step/', methods=['POST'])
+def step(instance_id):
     """
     Execute given action.
+
+    Accept json with the following format:
+    {
+      'action': 0,
+      'render': true
+    }
+
+    The API assumes that action is an integer. Render can be set to expose the rendered image. Render is optional.
+
+    :param instance_id:
     :param action: Integer value representing the action.
-    :return:
+    :return: json with environment response.
     """
-    render = request.args.get('render')
+    request_json = request.get_json()
 
     try:
-        observation, reward, done, info = env.step(action)
-
-        if render:
-            save_img(env.render(render_mode))
-
-        return jsonify({
-            'observation': observation.tolist(),
-            'reward': reward,
-            'done': done,
-            'info': info
-        })
+        action = request_json['action']
+        render = request_json.get('render')
+        environment_response = envs.step(instance_id, action, render)
+        return jsonify(environment_response)
     except AssertionError:
         raise InvalidUsage('Invalid Action')
+    except gym.error.ResetNeeded as ex:
+        raise InvalidUsage(ex.message)
 
 
-@app.route("/info")
-def info():
+@app.route('/v1/envs/<instance_id>/monitor/start/', methods=['POST'])
+def monitor_start(instance_id):
+    """
+    Start monitoring. Accept json with the following format:
+    {
+        'force': true,
+        'resume: false
+    }
+    :param instance_id:
+    :return:
+    """
+    request_data = request.get_json()
+
+    force = request_data.get('force', False)
+    resume = request_data.get('resume', False)
+
+    envs.monitor_start(instance_id, force, resume)
+    # NOTE: no video_callable implemented yet
+    return '', 204
+
+
+@app.route('/v1/envs/<instance_id>/monitor/close/', methods=['POST'])
+def monitor_close(instance_id):
+    """
+    Stop monitoring.
+    :param instance_id:
+    :return:
+    """
+    envs.monitor_close(instance_id)
+    return '', 204
+
+
+@app.route('/v1/envs/<instance_id>/upload/', methods=['POST'])
+def upload(instance_id):
+    """
+
+    :param instance_id:
+    :return:
+    """
+    try:
+        request_data = request.get_json()
+
+        algorithm_id = request_data.get('algorithm_id', None)
+        writeup = request_data.get('writeup', None)
+        api_key = request_data.get('api_key', None)
+        ignore_open_monitors = request_data.get('ignore_open_monitors', False)
+
+        envs.upload(instance_id, algorithm_id, writeup, api_key, ignore_open_monitors)
+
+        return '', 204
+    except Exception as ex:
+        raise InvalidUsage(ex.message)
+
+
+@app.route("/v1/envs/<instance_id>/info", methods=['GET'])
+def info(instance_id):
     """
     Expose useful information, such as: action_space, and observation_space.
     :return:
     """
-    return jsonify({
-        'action_space': str(env.action_space),
-        'observation_space': {
-            'shape': env.observation_space.shape,
-            'low': env.observation_space.low.tolist(),
-            'high': env.observation_space.high.tolist()
-        }
-    })
+    return jsonify(envs.info(instance_id))
 
 
-@app.route("/help")
+@app.route("/v1/help")
 def readme():
     """
     :return: Friendly start message.
@@ -112,15 +144,15 @@ def readme():
     def parse_doc(function):
         return function.__doc__.strip().split('\n')[0]
 
+    def parse_methods(methods):
+        return ','.join(methods)
+
     return jsonify({
         'available_commands': {
-            "/reset": parse_doc(reset),
-            "/step/<int:action>": parse_doc(step),
-            "/info": parse_doc(info),
-            "/help": parse_doc(readme)
-        }
-    })
+            rule.rule: {
+                'methods': parse_methods(rule.methods), 'doc': parse_doc(globals()[rule.endpoint])
+            } for rule in app.url_map.iter_rules() if rule.endpoint != 'static'}})
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
